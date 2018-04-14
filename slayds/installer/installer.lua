6727717043c497d503f15da62ff80da91b4a9f17
theslaymann <deucebroom@outlook.com>
2018-04-14 17:01:24 -0500

    Update installer.lua

--- slayds/installer/installer.lua
+++ slayds/installer/installer.lua
@@ -1,8 +1,25 @@
 local args = {...}
 local file = args[1]
 local path = http.get("https://theslaymann.github.io/slayds/".. file)
-program = fs.open("slayds", "w")
+if not fs.exists("slaydsfiles/program") then
+  if not fs.exists("slaydsfiles/") then
+    fs.makeDir("slaydsfiles/")
+  end
+  local program = fs.open("slaydsfiles/program", "w")
+  program.close()
+end
+program = fs.open("slaydsfiles/program", "w")
 program.write(path.readAll())
 program.close()
 path.close()
-print("Successfully downloaded the Slay Download Service")
+print("Program has finished downloading. Run it?")
+print("[Y] Run the program")
+print("[N] Don't but save it")
+local event, key = os.pullEvent("key")
+if key == keys.y then
+  shell.run("clear")
+  shell.run("slaydsfiles/program")
+end
+if key == keys.n then
+  print("File saved to slaydsfiles/program")
+end
