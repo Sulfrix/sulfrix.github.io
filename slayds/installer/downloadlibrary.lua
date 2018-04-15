64587e93b999647896ab40596f806f0ed1b096a4
theslaymann <deucebroom@outlook.com>
2018-04-15 10:20:19 -0500

    limited download library

--- slayds/installer/downloadlibrary.lua
+++ slayds/installer/downloadlibrary.lua
@@ -3,7 +3,7 @@ local file = args[1]
 local path = http.get("https://theslaymann.github.io/slayds/content/".. file)
 path = http.get("https://theslaymann.github.io/slayds/content/programs/".. file ..".lua")
 local destpath = args[2]
-program = fs.open(destpath, "w")
+program = fs.open("slaydsfiles/programfiles/".. destpath, "w")
 program.write(path.readAll())
 program.close()
 path.close()
