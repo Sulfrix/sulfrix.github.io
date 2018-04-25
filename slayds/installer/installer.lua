950303d72077af2384746b31375dc46b40a2da3d
theslaymann <deucebroom@outlook.com>
2018-04-24 20:04:20 -0500

    Update installer.lua

--- slayds/installer/installer.lua
+++ slayds/installer/installer.lua
@@ -1,6 +1,17 @@
 local args = {...}
 local action = args[1]
 local file = args[2]
+local destpath = args[3]
+if action = update then
+  file = "update"
+  action = "run"
+end
+if action = "" then
+  print("Usage:")
+  print("slayds run <file> - downloads a file to run")
+  print("slayds save <file> <path> - downloads a file and saves it to the path")
+  return
+end
 local path = http.get("https://theslaymann.github.io/slayds/content/programs/".. file ..".lua")
 print("Downloading ".. args[1] .."...")
 if not fs.exists("slaydsfiles/program") then
@@ -17,12 +28,11 @@ path.close()
 if action == "run" then
   shell.run("clear")
   shell.run("slaydsfiles/program")
+  fs.delete("slaydsfiles/program")
 end
 if action == "save" then
   shell.run("clear")
   path = http.get("https://theslaymann.github.io/slayds/content/programs/".. file ..".lua")
-  print("Enter the path for the program.")
-  local destpath = read()
   program = fs.open(destpath, "w")
   program.write(path.readAll())
   program.close()
