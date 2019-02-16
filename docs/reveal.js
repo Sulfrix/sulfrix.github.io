4910802cb12d71da463aa16bc62c3dac102a847d
theslaymann <deucebroom@outlook.com>
2019-02-16 12:25:58 -0600

    Update reveal.js

--- docs/reveal.js
+++ docs/reveal.js
@@ -2,10 +2,10 @@ function FigureItOut() {
 	var elmo = "The American Public School System!";
 	document.getElementById("answer").innerHTML = elmo;
 	document.getElementById("revealButton").innerHTML = "Hide answer";
-	document.getElementById("revealButton").onClick = "hideAnswer()";
+	document.getElementById("revealButton").onclick = "hideAnswer()";
 }
 function hideAnswer() {
 	document.getElementById("answer") = "";
 	document.getElementById("revealButton").innerHTML = "Reveal answer";
-	document.getELementById("revealButton").onClick = "FigureItOut()"
+	document.getELementById("revealButton").onclick = "FigureItOut()"
 }
