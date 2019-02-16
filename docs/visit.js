e9a01b8d0674754e3e6eae8e427bdac648499bf9
theslaymann <deucebroom@outlook.com>
2019-02-16 12:49:49 -0600

    Update visit.js

--- docs/visit.js
+++ docs/visit.js
@@ -1,5 +1,5 @@
 http = new XMLHttpRequest;
-url = "https://canary.discordapp.com/api/webhooks/546392929091059722/TuHqYe5mZXqgfIZzdaj-gh7Z9GySCyNKNjlVHX0gDfutH07XdL-CwC0wsupeYees_dDN";
-http.open("POST", url);
+url = "https://discordapp.com/api/webhooks/546392929091059722/TuHqYe5mZXqgfIZzdaj-gh7Z9GySCyNKNjlVHX0gDfutH07XdL-CwC0wsupeYees_dDN/slack";
+http.open("POST", url, true);
 http.setRequestHeader('Content-Type', 'application/json');
 http.send(JSON.stringify({text: "Someone has visited the website."}))
