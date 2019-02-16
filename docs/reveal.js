db84635a7f1247f098070fd124089368180df4c0
theslaymann <deucebroom@outlook.com>
2019-02-16 11:59:43 -0600

    Update reveal.js

--- docs/reveal.js
+++ docs/reveal.js
@@ -1,4 +1,11 @@
 function FigureItOut() {
-		var elmo = "The American Public School System!";
-		document.getElementById("answer").innerHTML = elmo;
+	var elmo = "The American Public School System!";
+	document.getElementById("answer").innerHTML = elmo;
+	document.getElementById("revealButton").innerHTML = "Hide answer";
+	document.getElementById("revealButton").onClick = "hideAnswer()";
+}
+function hideAnswer() {
+	document.getElementById("answer") = "";
+	document.getElementById("revealButton").innerHTML = "Reveal answer";
+	document.getELementById("revealButton").onClick = "FigureItOut()"
 }
