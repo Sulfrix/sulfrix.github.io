b289a0ef5cd66d44f95ef6a24723154ce799ac7d
theslaymann <deucebroom@outlook.com>
2019-07-28 14:33:25 -0500

    Polishing SlayDS

--- slayds/installer/installer.lua
+++ slayds/installer/installer.lua
@@ -1,6 +1,17 @@
 local args = {...}
-local action = args[0]
-local file = args[1]
+local action = args[1]
+local file = args[2]
+if action == nil then
+  print("Usage:")
+  print("slayds run <file>: Runs a file from SlayDS servers")
+  print("slayds save <file> <path>: Saves a file to your computer at <path>")
+  print("slayds run update: Updates SlayDS. Do this often.")
+  return
+end
+if file == nil then
+  print("Please enter the desired file to download/run")
+  return
+end
 local path = http.get("https://sulfrix.github.io/slayds/content/programs/".. file ..".lua")
 print("Downloading ".. args[1] .."...")
 if not fs.exists("slaydsfiles/program") then
@@ -20,6 +31,10 @@ if action == "run" then
   fs.delete("slaydsfiles/program")
 end
 if action == "save" then
+  if args[3] == nil then
+    print("Specify a path.")
+    return
+  end
   shell.run("clear")
   path = http.get("https://sulfrix.github.io/slayds/content/programs/".. file ..".lua")
   local destpath = args[3]
@@ -27,4 +42,3 @@ if action == "save" then
   program.write(path.readAll())
   program.close()
 end
-path.close()
