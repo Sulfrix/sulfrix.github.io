266be64e035e9931998b1b562a85054bcab80ce4
theslaymann <deucebroom@outlook.com>
2019-12-20 19:20:25 -0600

    Updates, revamped gmod loading screen for use with servers

---
+++ gmodloading.js
@@ -0,0 +1,36 @@
+var music = new Audio('/gmodLoadMusic.mp3')
+
+music.play()
+
+var fields = {
+    serverName: document.getElementById('ServerName'),
+    loadingStatus: document.getElementById('LoadingStatus'),
+    filesNeeded: 0,
+    currentFiles: 0,
+}
+
+
+function GameDetails(servername, serverurl, mapname, maxplayers, steamid, gamemode) {
+    fields.serverName.innerHTML = servername
+}
+
+function SetStatusChanged(status) {
+    fields.loadingStatus.innerHTML = status
+}
+
+function SetFilesTotal(total) {
+    fields.currentFiles = total
+}
+
+function SetFilesNeeded(needed) {
+    fields.filesNeeded = needed
+    refreshProgress()
+}
+
+function refreshProgress() {
+    var calculated
+    let currentFiles = fields.currentFiles
+    let filesNeeded = fields.filesNeeded
+    calculated = ((currentFiles - filesNeeded) / currentFiles) * 100
+    document.getElementById('progress').style = `width: ${calculated}%`
+}
