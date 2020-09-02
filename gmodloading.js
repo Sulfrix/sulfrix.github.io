ee438f3b733a9c7b2b810acc79ad93dbac933b85
Sulfrix <deucebroom@outlook.com>
2020-09-02 10:24:45 -0500

    bruh

--- gmodloading.js
+++ gmodloading.js
@@ -1,55 +1,62 @@
-var music = new Audio('/gmodloadMusic.mp3')
-music.volume = 0.25;
-music.play()
+var music = new Audio("/gmodloadMusic.mp3");
+music.volume = 0.5;
+music.play();
 
 if (outerWidth > 2560 && outerHeight > 1440) {
-    document.all[0].setAttribute("style", "zoom: 200%")
+	document.all[0].setAttribute("style", "zoom: 150%");
 }
 
 function miniconsole(text, hoverText) {
-    let ele = document.createElement("div")
-    ele.className = "logMessage"
-    let hoverMessage = document.createElement("div")
-    hoverMessage.className = "logHover"
-    hoverMessage.innerHTML = hoverText
-    let anchor = document.getElementById('anchor')
-    ele.innerHTML = text
-    document.querySelector(".miniConsole").insertBefore(ele, anchor)
-    ele.appendChild(hoverMessage)
-    ele.scrollIntoView()
+	let ele = document.createElement("div");
+	ele.className = "logMessage";
+	let hoverMessage = document.createElement("div");
+	hoverMessage.className = "logHover";
+	//hoverMessage.innerHTML = hoverText
+	let anchor = document.getElementById("anchor");
+	ele.innerHTML = text;
+	document.querySelector(".miniConsole").insertBefore(ele, anchor);
+	ele.appendChild(hoverMessage);
+	ele.scrollIntoView();
 }
 
 var fields = {
-    serverName: document.getElementById('ServerName'),
-    loadingStatus: document.getElementById('LoadingStatus'),
-    filesNeeded: 0,
-    currentFiles: 0,
-}
+	serverName: document.getElementById("ServerName"),
+	loadingStatus: document.getElementById("LoadingStatus"),
+	filesNeeded: 0,
+	currentFiles: 0,
+};
 
-
-function GameDetails(servername, serverurl, mapname, maxplayers, steamid, gamemode) {
-    fields.serverName.innerHTML = servername
-    miniconsole("Welcome to " + servername, "GameDetails()")
+function GameDetails(
+	servername,
+	serverurl,
+	mapname,
+	maxplayers,
+	steamid,
+	gamemode
+) {
+	fields.serverName.innerHTML = servername;
+	miniconsole("Welcome to " + servername, "GameDetails()");
+	miniconsole("Current Map: " + mapname);
 }
 
 function SetStatusChanged(status) {
-    fields.loadingStatus.innerHTML = status
-    miniconsole(status, "SetStatusChanged()")
+	fields.loadingStatus.innerHTML = status;
+	miniconsole(status, "SetStatusChanged()");
 }
 
 function SetFilesTotal(total) {
-    fields.currentFiles = total
+	fields.currentFiles = total;
 }
 
 function SetFilesNeeded(needed) {
-    fields.filesNeeded = needed
-    refreshProgress()
+	fields.filesNeeded = needed;
+	refreshProgress();
 }
 
 function refreshProgress() {
-    var calculated
-    let currentFiles = fields.currentFiles
-    let filesNeeded = fields.filesNeeded
-    calculated = ((currentFiles - filesNeeded) / currentFiles) * 100
-    document.getElementById('progress').style = `width: ${calculated}%`
+	var calculated;
+	let currentFiles = fields.currentFiles;
+	let filesNeeded = fields.filesNeeded;
+	calculated = ((currentFiles - filesNeeded) / currentFiles) * 100;
+	document.getElementById("progress").style = `width: ${calculated}%`;
 }
